d654906e56e3c55736431e4b62dcc7555e5249be
schueler <schueler@ubuntu>
2018-06-11 14:07:07 +0200

    5th commit

--- Test1/src/pkgGUI/MainGUI.java
+++ Test1/src/pkgGUI/MainGUI.java
@@ -1,7 +1,13 @@
 package pkgGUI;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 
 public class MainGUI extends JFrame {
 	JButton btnOK;
+	
+	
+	JMenu menu1;
+	JMenu menu2;
+	JMenu menu3;
 }
